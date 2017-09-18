e4fabeb06e7b5c582e12e56ddac2adec0ba2ee0c
pbzweihander <sd852456@naver.com>
2017-09-19 00:29:19 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#[macro_use]
+extern crate serde_derive;
+
+pub mod irc;
+pub mod slack;
+pub mod config;
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+extern crate serde;
+extern crate serde_json;
+extern crate echobot;
+
+use echobot::slack::Slack;
+use echobot::irc::IRC;
+use echobot::config::*;
+use std::fs::File;
+use std::env;
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let config_file_path = if args.len() > 1 {
+        &args[1]
+    } else {
+        "config.json"
+    };
+    let config_file = File::open(config_file_path).unwrap();
+    let config: Config = serde_json::from_reader(config_file).unwrap();
+
+    let mut irc = IRC::new(&config.irc.server, &config.irc.nickname).unwrap();
+    irc.join_multi(&config.irc.channels.iter().map(|s| s.as_ref()).collect::<Vec<_>>()).unwrap();
+
+    for line in irc {
+        println!("{}", line);
+    }
+
+    // let slack = Slack::new(&config.slack.token).unwrap();
+    // let rtm = slack.rtm_connect().unwrap();
+
+    // for m in slack.users_list().unwrap() {
+    //     println!("{}", m.name);
+    // }
+
+    // for c in slack.channels_list().unwrap() {
+    //     println!("{}", c.name);
+    // }
+
+    // for l in rtm {
+    //     println!("{}", l.text);
+    // }
+}
