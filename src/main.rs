964fd77ca28ecf50b385be8607cc253cac117349
pbzweihander <sd852456@naver.com>
2017-09-19 18:31:14 +0900

    pretty error message

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,52 @@ extern crate serde;
 extern crate serde_json;
 extern crate echobot;
 
-use echobot::slack::Slack;
-use echobot::irc::IRC;
-use echobot::config::*;
+use std::error::Error;
 use std::fs::File;
 use std::env;
 
-fn main() {
+use echobot::slack::Slack;
+use echobot::irc::IRC;
+use echobot::config::*;
+
+fn init() -> Result<(), Box<Error>> {
     let args: Vec<_> = env::args().collect();
     let config_file_path = if args.len() > 1 {
         &args[1]
     } else {
         "config.json"
     };
-    let config_file = File::open(config_file_path).unwrap();
-    let config: Config = serde_json::from_reader(config_file).unwrap();
+    let config_file = File::open(config_file_path)?;
+    let config: Config = serde_json::from_reader(config_file)?;
 
-    let mut irc = IRC::new(&config.irc.server, &config.irc.nickname).unwrap();
-    irc.join_multi(&config.irc.channels.iter().map(AsRef::as_ref).collect::<Vec<_>>()).unwrap();
+    let mut irc = IRC::new(&config.irc.server, &config.irc.nickname)?;
+    irc.join_multi(&config.irc.channels.iter().map(AsRef::as_ref).collect::<Vec<_>>())?;
 
     for line in irc {
         println!("{}", line);
     }
 
-    // let slack = Slack::new(&config.slack.token).unwrap();
-    // let rtm = slack.rtm_connect().unwrap();
+    // let slack = Slack::new(&config.slack.token)?;
+    // let rtm = slack.rtm_connect()?;
 
-    // for m in slack.users_list().unwrap() {
+    // for m in slack.users_list()? {
     //     println!("{}", m.name);
     // }
 
-    // for c in slack.channels_list().unwrap() {
+    // for c in slack.channels_list()? {
     //     println!("{}", c.name);
     // }
 
     // for l in rtm {
     //     println!("{}", l.text);
     // }
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = init() {
+        eprintln!("Application error: {}", e);
+        std::process::exit(1);
+    }
 }
