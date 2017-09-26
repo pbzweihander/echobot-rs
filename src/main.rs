81aa4a5c012063b241b23b484850ad378ed9a671
pbzweihander <sd852456@naver.com>
2017-09-26 19:28:04 +0900

    4 threads

--- src/main.rs
+++ src/main.rs
@@ -24,47 +24,68 @@ fn init() -> Result<(), Box<Error>> {
     let config_file = File::open(config_file_path)?;
     let config: Config = serde_json::from_reader(config_file)?;
 
-    let (irc_message_tx, irc_message_rx): (mpsc::Sender<irc::Message>, mpsc::Receiver<irc::Message>) = mpsc::channel();
-    let (slack_message_tx, slack_message_rx): (mpsc::Sender<slack::Message>, mpsc::Receiver<slack::Message>) = mpsc::channel();
+    let (irc_message_tx, irc_message_rx): (mpsc::Sender<irc::Message>,
+                                           mpsc::Receiver<irc::Message>) = mpsc::channel();
+    let (slack_message_tx, slack_message_rx): (mpsc::Sender<slack::Message>,
+                                               mpsc::Receiver<slack::Message>) = mpsc::channel();
 
     let irc_channel = config.irc.channel.clone();
+    let irc_channel2 = irc_channel.clone();
     let slack_channel = config.slack.channel.clone();
+    let slack_channel2 = slack_channel.clone();
 
     let mut irc: IRC = IRC::new(&config.irc.server, &config.irc.nickname)?;
 
     let mut slack: Slack = Slack::new(&config.slack.token)?;
-    let mut rtm: Peekable<slack::SlackRTM> = slack.request.rtm_connect()?;
+    let rtm: Peekable<slack::SlackRTM> = slack.request.rtm_connect()?;
 
     irc.join(&irc_channel)?;
+    irc.privmsg(&irc_channel, "hi!")?;
 
-    let irc_thread = thread::Builder::new().name("IRC Thread".to_owned()).spawn(move || loop {
-        if irc.peek().is_some() {
-            let m = irc.next().unwrap();
+    let slack2 = slack.clone();
+    let mut irc2 = irc.try_clone()?;
+
+    let irc_rx_thread = thread::Builder::new()
+        .name("IRC Receiver Thread".to_owned())
+        .spawn(move || for m in irc {
             println!("IRC: <{}> {}", m.user, m.text);
             if irc_channel == m.channel {
                 irc_message_tx.send(m).unwrap();
             }
-            for m in slack_message_rx.try_iter() {
-                irc.privmsg(&irc_channel, &format!("<{}> {}", m.user.name, m.text)).unwrap();
-            }
-        }
-    })?;
+        })?;
 
-    let slack_thread = thread::Builder::new().name("Slack Thread".to_owned()).spawn(move || loop {
-        if rtm.peek().is_some() {
-            let m = slack.raw_message_to_message(rtm.next().unwrap()).unwrap();
-            println!("Slk: <{}> {}", m.user.name, m.text);
-            if slack_channel == m.channel.name {
-                slack_message_tx.send(m).unwrap();
-            }
-            for m in irc_message_rx.try_iter() {
-                slack.request.chat_post_message(&slack_channel, &format!("<{}> {}", m.user, m.text)).unwrap();
-            }
-        }
-    })?;
+    let irc_tx_thread = thread::Builder::new()
+        .name("IRC Transmitter Thread".to_owned())
+        .spawn(move || for m in slack_message_rx.iter() {
+            irc2.privmsg(&irc_channel2, &format!("<{}> {}", m.user.name, m.text))
+                .unwrap();
+            println!("Slk -> IRC: <{}> {}", m.user.name, m.text);
+        })?;
 
-    irc_thread.join().ok();
-    slack_thread.join().ok();
+    let slack_rx_thread = thread::Builder::new()
+        .name("Slack Receiver Thread".to_owned())
+        .spawn(move || for m in rtm {
+            let parsed = slack.raw_message_to_message(m).unwrap();
+            println!("Slk: <{}> {}", parsed.user.name, parsed.text);
+            if slack_channel == parsed.channel.name {
+                slack_message_tx.send(parsed).unwrap();
+            }
+        })?;
+
+    let slack_tx_thread = thread::Builder::new()
+        .name("Slack Transmitter Thread".to_owned())
+        .spawn(move || for m in irc_message_rx.iter() {
+            slack2
+                .request
+                .chat_post_message(&slack_channel2, &format!("<{}> {}", m.user, m.text))
+                .unwrap();
+            println!("IRC -> Slk: <{}> {}", m.user, m.text);
+        })?;
+
+    irc_rx_thread.join().ok();
+    irc_tx_thread.join().ok();
+    slack_rx_thread.join().ok();
+    slack_tx_thread.join().ok();
 
     Ok(())
 }
